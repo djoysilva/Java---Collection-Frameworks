78fc78cfad41187b9a844538b5c9f9fb90880a27
Joyce Mara da Silva <RM76983@fiap.com.br>
2016-10-04 09:05:35 -0300

    ask the user and pass in the test, dao and bo class

--- OrdemObjetos/src/br/com/fiap/bo/CargoBO.java
+++ OrdemObjetos/src/br/com/fiap/bo/CargoBO.java
@@ -7,27 +7,30 @@ import br.com.fiap.dao.CargoDAO;
 import br.com.fiap.excecao.Excecao;
 
 public class CargoBO{
-	public void novoCargo(Cargo c) throws Exception{
+	public void novoCargo(Cargo c, String u, String p) throws Exception{
 		if(c.getCargo().length()<2){
 			throw new Excecao("Caracteres insuficientes no Cargo");
 		}
 		if(c.getSalario()<700){
 			throw new Excecao("Salário não pode ser menor que 700");
 		}
-		new CargoDAO().gravar(c);
+		new CargoDAO(u,p).gravar(c);
 	}
-	public List<Cargo> listarCargos() throws Exception{
-		return new CargoDAO().getLista();
-	}
-	
-	public Cargo consultarCargo(String pCargo) throws Exception{
-		return new CargoDAO().getPesquisarCargo(pCargo.toUpperCase());
+	public List<Cargo> listarCargos(String u, String p) throws Exception{
+		return new CargoDAO(u,p).getLista();
 	}
 	
-	public void deletar(String pCargo) throws Exception{
+	public Cargo consultarCargo(String pCargo, String u, String p) throws Exception{
+		return new CargoDAO(u, p).getPesquisarCargo(pCargo.toUpperCase());
+	}
+	
+	public void deletar(String pCargo, String u, String p) throws Exception{
 		if(pCargo.length()<2){
 			throw new Excecao("Caracteres insuficientes no cargo");
 		}
-		new CargoDAO().deletar(pCargo);
+		new CargoDAO(u, p).deletar(pCargo);
+	}
+	public int atualizarSalario(double pAumento, String u, String p) throws Exception{
+		return new CargoDAO(u,p).atualizar(pAumento);
 	}
 }
--- OrdemObjetos/src/br/com/fiap/dao/CargoDAO.java
+++ OrdemObjetos/src/br/com/fiap/dao/CargoDAO.java
@@ -9,14 +9,36 @@ import java.util.List;
 import br.com.fiap.beans.Cargo;
 import br.com.fiap.conexao.ConexaoFactory;
 
+/**
+ * Na documentaçã você inicia desenvolvendo uma aplicação sucinta do se projeto. Neste espaço onde você
+ * pode utilizar as tags de HTML, como por exemplo, a <code. tag </code>, ou ainda deixar em <i>itálico
+ * </i> e etc.
+ * Existem ainda marcas especiais como:
+ * @author Joyce Mara da Silva
+ * @version1.5
+ * @since1.0
+ * @see OutraClasseRelacionada
+ * @see OutraClasseRelacionadaComEsteProjeto
+ * Não deixe também de comentar os seus códigos, principalmente os métodos com a mesma
+ * <b>estrutura</b>
+ * */
+
 public class CargoDAO {
 	
 	private Connection conexao;
 	
-	public CargoDAO() throws Exception{
-		this.conexao = new ConexaoFactory().getConnection();
+	public CargoDAO(String user, String pass) throws Exception{
+		this.conexao = new ConexaoFactory().getConnection(user, pass);
 	}
 	
+	/**
+	 * Método que <b>adiciona</b> ums tupla na tabela
+	 * TB_CARGO.
+	 * @param c Um objet d tipo Cargo
+	 * @return sem retorno
+	 * @author Joyce Silva
+	 * @see Cargos
+	 */
 	public void gravar(Cargo c) throws Exception{
 		String sql = "insert into TB_CARGO " +
 					"(NM_CARGO, DS_NIVEL, VL_SALARIO) values (?, ?, ?)";
@@ -65,6 +87,14 @@ public class CargoDAO {
 		estrutura.setString(1, strCargo);
 		estrutura.execute();
 		estrutura.close();	
+	}
+	
+	public int atualizar(double pAumento) throws Exception{
+		PreparedStatement stmt = conexao.prepareStatement("update TB_CARGO set VL_SALARIO=VL_SAARIO * ('+?)");
+		stmt.setDouble(1, pAumento/100);
+		int saida = stmt.executeUpdate();
+		stmt.close();
+		return saida;
 	}
 	
 }
